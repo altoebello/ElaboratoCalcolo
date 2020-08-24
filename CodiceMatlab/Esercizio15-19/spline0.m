5855350ee22e1f14be3b7daeacd0dd29f51b3780
altoebello <mhong1654@gmailcom>
2020-08-24 17:13:17 +0200

    modifiche alle cartelle

--- CodiceMatlab/Esercizio15-19/spline0.m
+++ CodiceMatlab/Esercizio15-19/spline0.m
@@ -1,6 +1,6 @@
-function x = spline0( xi, fi )
+function y = spline0( xi, fi,x )
 %
-% mi = spline0( xi, fi ) Calcola il vettore degli mi per il calcolo di una
+% y = spline0( xi, fi ) Calcola il vettore degli mi per il calcolo di una
 % spline cubica naturale interpolante i punti (xi,fi).
 %
     m = length(xi);
@@ -22,5 +22,12 @@ function x = spline0( xi, fi )
     csi = csi(1:n-2);
     mi = trisolve( phi, d, csi, rhs );
     mi = [0;mi;0];
+    r=fi(1:n)-((hi(1:n).^2)/6).*mi(1:n);
+    q=df(1:n)-((hi(1:n)/6).*(mi(2:m)-mi(1:n)));
+    len = length(x);
+    y = zeros(len,1);
+    for i = 1 : len-1
+        y(i) =((( x - xi(i)).^3.*mi(i+1) + (xi(i+1)-x).^3.*mi(i))/6*hi(i+1) + q(i+1).*(x-xi(i)+r(i+1)));
+    end
     return
 end
